5b16077364519e68e9c072534770e4febc08a0aa
Ryudas <sivlesegrob9@gmail.com>
2020-08-24 16:20:57 +0200

    new test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git pull 
-sudo rm -rf /data/bucket/dpi/images/fdde99a4-d6b0-4bc1-adbf-234f65cfc17a
-docker rm -v pigen_fdde99a4-d6b0-4bc1-adbf-234f65cfc17a_work
+sudo rm -rf /data/bucket/dpi/images/*
+docker rm -v pigen_*
 docker-compose up -d -V --build dpi-api
