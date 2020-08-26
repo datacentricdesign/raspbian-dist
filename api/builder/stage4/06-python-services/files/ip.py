e4b3223857e46d3ca0aeeb179f37c6f8b33936ef
Jacky Bourgeois <J.Bourgeois@tudelft.nl>
2020-08-26 21:33:13 +0200

    update cpu/ip script

--- api/builder/stage4/06-python-services/files/ip.py
+++ api/builder/stage4/06-python-services/files/ip.py
@@ -2,37 +2,24 @@ from dotenv import load_dotenv
 import os
 
 from dcd.entities.thing import Thing
-from dcd.entities.property import PropertyType
 from time import sleep
 
 # The thing ID and access token
 load_dotenv()
 THING_ID = os.environ['THING_ID']
-THING_TOKEN = os.environ['THING_TOKEN']
 
 # Instantiate a thing with its credential
-my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
+my_thing = Thing(thing_id=THING_ID, private_key_path="/etc/ssl/certs/" + THING_ID + "private.pem")
 
-# We can fetch the details of our thing
-my_thing.read()
-my_property = my_thing.find_or_create_property("IP Address", PropertyType.TEXT)
-my_property_cpu = my_thing.find_or_create_property("Processor Usage", PropertyType.TEXT)
-
-def update_ip():
-    f = os.popen('ifconfig wlan0 | grep "inet " | cut -d " " -f10')
-    ip = f.read()
-    my_property.update_values((ip.rstrip(),))
-
+# Find or create a property to store processor usage
+my_property_cpu = my_thing.find_or_create_property("Processor Usage", "CPU")
 
 def update_cpu():
 	f = os.popen('top -bn1 | grep "Cpu" | cut -c 10-13')
 	cpu = f.read()
-    my_property_cpu.update_values((cpu.rstrip(),))	
+	my_property_cpu.update_values((cpu.rstrip(),))	
 
-
-update_ip()
-
-while true:
+while True:
 	update_cpu()
 	# sleep every 60 seconds
 	time.sleep(60)
