cc9f4c3d7269d403a5df0a45dededdf71322ed1c
Ryudas <Ryudas@users.noreply.github.com>
2020-08-27 12:34:27 +0200

    updated shell running with private key loading

--- api/builder/stage4/06-python-services/00-run.sh
+++ api/builder/stage4/06-python-services/00-run.sh
@@ -6,12 +6,28 @@ install -v -m 755 files/ip.py	       "${ROOTFS_DIR}/etc/systemd/system/service_s
 
 # update .env file 
 cat > files/.env << EOF
-THING_ID=${THING_ID}
-THING_TOKEN=${THING_TOKEN}
+THING_ID=dcd:things:${ID}
+
 EOF
 
-
 install -v -m 644 files/.env	       "${ROOTFS_DIR}/etc/systemd/system/service_scripts/python/"	
+
+# create private key 
+touch files/${ID}.private.pem
+
+cat > files/${ID}.private.pem << EOF
+${PRIVATE_KEY}
+EOF
+
+# place private key 
+install -v -d                          "${ROOTFS_DIR}/etc/ssl/certs"
+install -v -m 644 files/${ID}.private.pem	       "${ROOTFS_DIR}/etc/ssl/certs/"
+
+# run chown
+on_chroot << EOF
+chown root:root /etc/ssl/certs/${ID.private.pem}
+EOF
+
 
 # place ip service
 install -v -m 644 files/ip.service       "${ROOTFS_DIR}/etc/systemd/system/"
