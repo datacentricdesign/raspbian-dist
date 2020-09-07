45a26e09ddc742097603685a9062d42563ae28a0
Ryudas <sivlesegrob9@gmail.com>
2020-09-07 22:16:39 +0200

    updated eduroam services

--- builder/build.sh
+++ builder/build.sh
@@ -244,8 +244,6 @@ if [[ -n "${WPA_PASSWORD}" && ${#WPA_PASSWORD} -lt 8 || ${#WPA_PASSWORD} -gt 63 
 	exit 1
 fi
 
-echo ${HOME_PASSWORD}
-echo ${HOME_ESSID}
 
 mkdir -p "${WORK_DIR}"
 log "Begin ${BASE_DIR}"
--- builder/stage4/06-python-services/00-run.sh
+++ builder/stage4/06-python-services/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 # place scripts services depend on
+mkdir -p "${ROOTFS_DIR}/etc/systemd/system/service_scripts/python"
 install -v -d                          "${ROOTFS_DIR}/etc/systemd/system/service_scripts/python"
 install -v -m 755 files/ip.py	       "${ROOTFS_DIR}/etc/systemd/system/service_scripts/python/"
 
@@ -30,6 +31,26 @@ chown root:root   "${ROOTFS_DIR}/etc/ssl/certs/dcd:things:${ID}.private.pem"
 
 # place ip service
 install -v -m 644 files/ip.service       "${ROOTFS_DIR}/etc/systemd/system/"
+
+cat > "${ROOTFS_DIR}/etc/systemd/system/ip.service" << EOF
+[Unit]
+  Description=Automatically send device IP to DCD hub using a python sdk
+  Wants=network-online.target
+  After=network-online.target
+
+  [Service]
+
+  ExecStart=/usr/bin/python3.7 /etc/systemd/system/service_scripts/python/ip.py
+  StandardOutput=syslog 
+  StandardError=syslog 
+  Restart=always
+  RestartSec=10
+  User=${FIRST_USER_NAME}
+
+  [Install]
+  WantedBy=multi-user.target
+EOF
+
 
 
 # install python services 
