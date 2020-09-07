74ce18d830aab5825b33096f94bbe52ccbbcc70f
Ryudas <sivlesegrob9@gmail.com>
2020-09-07 14:08:49 +0200

    Updated configs

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -7,6 +7,7 @@ install -v -d                             "${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf      "${ROOTFS_DIR}/etc/wpa_supplicant/"
 
 # place scripts services depend on
+mkdir -p "${ROOTFS_DIR}/etc/systemd/system/service_scripts"
 install -v -d                             "${ROOTFS_DIR}/etc/systemd/system/service_scripts"
 install -v -m 755 files/eduroam.sh	       "${ROOTFS_DIR}/etc/systemd/system/service_scripts/"	
 
@@ -20,7 +21,28 @@ install -v -m 644 files/eduroam.service       "${ROOTFS_DIR}/etc/systemd/system/
 #  echo "country=${WPA_COUNTRY}" >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"
 # fi
 
+# set eduroam service 
+cat > "${ROOTFS_DIR}/etc/systemd/system/eduroam.service" << EOL
+[Unit]
+Description=Connect to eduroam automatically using older driver
+After=network.target
 
+[Service]
+
+ExecStart=/bin/bash /etc/systemd/system/service_scripts/eduroam.sh
+StandardOutput=syslog 
+StandardError=syslog 
+Restart=always
+RestartSec=10
+User=${FIRST_USER_NAME}
+
+[Install]
+WantedBy=multi-user.target
+
+EOL
+
+
+# IF WPA SET 
 if [ -v WPA_ESSID ] && [ -v WPA_PASSWORD ]; then
 HASH=$(echo -n ${WPA_PASSWORD} | iconv -t UTF-16LE | openssl md4  -binary | xxd -p)
 cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf" << EOL
@@ -40,33 +62,42 @@ network={
        
 }
 
-
-EOL
-
-HASH=$(echo -n ${HOME_PASSWORD} | iconv -t UTF-16LE | openssl md4  -binary | xxd -p)
-
-cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf" << EOL
-
-network={
-       ssid="${HOME_ESSID}"
-       password="${HOME_PASSWORD}" 
-}
-
-
 EOL
 
 fi
 
+# if home network 
+#if [ -v HOME_ESSID ] && [ -v HOME_PASSWORD ]; then
+#echo "HOME ESSID SET"
+# home network 
+#NET=$(wpa_passphrase "${HOME_ESSID}" "${HOME_PASSWORD}" | sed -n '/#psk/!p' ) 
+
+#echo ${NET}
+
+cat >> "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf" << EOF
+
+network={
+       ssid="${HOME_ESSID}"
+       psk="${HOME_PASSWORD}"       
+}
+
+EOF
+
+echo "Completed"
+
+#fi
 
 # Disable wifi on 5GHz models
-mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
+#mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
 #echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
 #echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
 
-
-# Activate all services 
-# install python services 
+# enable service is not home set 
+if ! [ -v HOME_ESSID ] && ! [ -v HOME_PASSWORD ]; then
+# no home esssid
 on_chroot << EOF
 systemctl enable eduroam.service
 
 EOF
+
+fi
